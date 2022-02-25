ecff689ef0d41ec4bcf7719b13f85db64955e57d
hagenh <haakonhagen@live.no>
2022-02-25 15:50:53 +0100

    First commit + test github actions

--- src/main/java/no/pgr301/konte/IOTController.java
+++ src/main/java/no/pgr301/konte/IOTController.java
@@ -1,6 +1,7 @@
 package no.pgr301.konte;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,12 @@ public class IOTController {
     }
 
     // Todo implement an endpoint that returns all measurements
+    @GetMapping(path = "/weather", consumes = "application/json")
+    public ResponseEntity<List<Measurement>> getMeasure(
+            @RequestBody Measurement measurement) {
+        //measurementList.add(measurement);
+        return ResponseEntity.of(Optional.of(measurementList));
+    }
 
 
 }
--- src/test/java/no/pgr301/konte/KonteApplicationTests.java
+++ src/test/java/no/pgr301/konte/KonteApplicationTests.java
@@ -3,11 +3,22 @@ package no.pgr301.konte;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 class KonteApplicationTests {
 
+	private final List<Measurement> measurementList = new ArrayList<>();
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	boolean testWillFail(){
+		//measurementList.size() == 2;
+		return measurementList.size() == 2;
 	}
 
 }
